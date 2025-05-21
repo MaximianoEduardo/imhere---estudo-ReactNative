2782ef183208c0db387d2c0087c8eabd5d0eecf8
emaximiano <edu.maxi.96@gmail.com>
2025-05-21 14:27:32 -0300

    finalizando app

--- App.tsx
+++ App.tsx
@@ -1,6 +1,14 @@
+import { StatusBar, Text, View } from "react-native";
+import HomeScreen from "./src/screens/Home";
 
 export default function App() {
   return (
+    <>
+      <StatusBar barStyle={"light-content"}/>
+      <HomeScreen/>
+    </>
+
+    
 
   );
 }
